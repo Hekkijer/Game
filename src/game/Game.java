67ed15e4875ceda58bce13e0b359f2f9b9d73c89
Your Name <you@example.com>
2021-09-17 18:56:52 +0300

    added try again function

--- src/game/Game.java
+++ src/game/Game.java
@@ -19,29 +19,44 @@ public class Game {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        Random random = new Random();
-        int num = random.nextInt(5 - 0 + 1) + 0;
-        Scanner in = new Scanner(System.in);
         
-        int count = 3;
-        for (int i = 1; i <= 3; i++) {
-            System.out.println("You have "+count+" lifes");
-            System.out.println("Choose number from 0 to 5: ");
-            int number = in.nextInt();
+        while (true) {
+            Random random = new Random();
+            int num = random.nextInt(5 - 0 + 1) + 0;
+            Scanner in = new Scanner(System.in);
 
-            if (num == number) {
-                System.out.print("Yes, yo won");
+            int count = 3;
+            for (int i = 1; i <= 3; i++) {
+                System.out.println("You have " + count + " lifes");
+                System.out.println("Choose number from 0 to 5: ");
+                int number = in.nextInt();
+
+                if (num == number) {
+                    System.out.print("Yes, yo won \n");
+                    break;
+                } else {
+                    System.out.println("No, you lost");
+                    count -= 1;
+                }
+            }
+
+            if (count == 0) {
+                System.out.println("Thats all, you dont have any lifes");
+                System.out.println("The number was " + num);
+            }
+            System.out.println("Do you want to try again? y/n: ");
+            in = new Scanner(System.in);
+            String answer = in.nextLine();
+            
+            if (answer.equals("n")) {
                 break;
-            } else {
-                System.out.println("No, you lost");
-                count -= 1;
+            }
+            if (!(answer.equals("n") && answer.equals("y"))) {
+                System.out.println("Uknown character. Game stopped");
+                break;
             }
         }
         
-        if (count == 0) {
-            System.out.println("Thats all, you dont have any lifes");
-            System.out.println("The number was "+num);
-        }
         
     }
     
